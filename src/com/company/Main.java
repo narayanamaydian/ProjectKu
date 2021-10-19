3049904e775c7676e46f6c7ed3ed711673232864
Narayana Maydian <narayanamaydian@gmail.com>
2021-10-19 20:49:16 +0700

    Implements Stream

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        List<Integer> number = new ArrayList<Integer>();
+
+        List<Integer> anotherNum = new ArrayList<Integer>();
+
+        for(int i = 1; i<11; i++){
+            number.add(i);
+        }
+
+        for(int i = 0; i<number.size(); i++){
+            System.out.println(number.get(i));
+        }
+        System.out.println("menggunakan stream");
+        // number.stream().forEach(currNum -> System.out.println(currNum));
+
+        anotherNum = number.stream().filter(currNum -> currNum % 2 == 0).collect(Collectors.toList());
+
+        anotherNum.stream().forEach(currNum -> System.out.println(currNum));
+
     }
 }
